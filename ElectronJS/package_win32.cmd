ce68dff2c7ff563f8682eea6f2da577f7432cd15
naibo <naibowang@u.nus.edu>
2023-03-24 02:53:04 +0800

    Win32 update

--- ElectronJS/package_win32.cmd
+++ ElectronJS/package_win32.cmd
@@ -1,17 +1,2 @@
-@echo off
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-goto UACPrompt
-) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-exit /B
-:gotAdmin
-if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-pushd "%CD%"
-CD /D "%~dp0"
-
 rmdir /s /q out
 npm run package
