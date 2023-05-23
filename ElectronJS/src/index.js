76fe3a83b92246a14e5208b69bce810f0dc73c2d
naibo <naibowang@foxmail.com>
2023-05-24 04:27:46 +0800

    Wait for TEST and ENG Version

--- ElectronJS/src/index.js
+++ ElectronJS/src/index.js
@@ -11,65 +11,64 @@ const i18n = VueI18n.createI18n({
 })
 
 
-    function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg); //匹配目标参数
-        if (r != null) return unescape(r[2]);
-        return "";
-    }
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return "";
+}
 
-    var app = Vue.createApp({
-        data() {
-            return {
-                init: true,
-                lang: 'zh',
-                user_data_folder: getUrlParam("user_data_folder"),
-                step: 0,
-                newest_version: '-', // 最新版本号
-            }
-        },
-        mounted() {
-            // 发送GET请求获取GitHub的Release API响应
-            const request = new XMLHttpRequest();
-            request.open('GET', `https://api.github.com/repos/NaiboWang/EasySpider/releases/latest`);
-            request.setRequestHeader('User-Agent', 'JavaScript');
-            request.onload = function() {
-                // 解析响应JSON并输出最新版本号
-                const release = JSON.parse(request.responseText);
-                const latestVersion = release.tag_name;
-                app.$data.newest_version = latestVersion;
-                // alert(`Latest version is ${latestVersion}`);
-            };
-            request.onerror = function() {
-                console.error('Error: failed to get latest version.');
-            };
-            request.send();
-        },
-        methods: {
-            changeLang(lang = 'zh') {
-                this.init = false;
-                this.lang = lang;
-            },
-            startDesign(lang, with_data = false) {
-                if (with_data) {
-                    console.log(this.user_data_folder)
-                    if (this.user_data_folder == null || this.user_data_folder == "") {
-                        if (lang == 'zh') {
-                            alert("请指定用户信息目录");
-                        } else {
-                            alert("Please specify the user information directory");
-                        }
-                        return;
-                    }
-                    window.electronAPI.startDesign(lang, this.user_data_folder);
-                } else {
-                    window.electronAPI.startDesign(lang);
-                }
-            },
-            startInvoke(lang) {
-                window.electronAPI.startInvoke(lang);
-            }
+var app = Vue.createApp({
+    data() {
+        return {
+            init: true,
+            lang: 'zh',
+            user_data_folder: getUrlParam("user_data_folder"),
+            step: 0,
+            newest_version: '-', // 最新版本号
         }
-    }).use(i18n).mount('#app')
-    console.log(i18n.global.t('test'))
-    document.title = i18n.global.t('test');
+    },
+    mounted() {
+        // 发送GET请求获取GitHub的Release API响应
+        const request = new XMLHttpRequest();
+        request.open('GET', `https://api.github.com/repos/NaiboWang/EasySpider/releases/latest`);
+        request.setRequestHeader('User-Agent', 'JavaScript');
+        request.onload = function() {
+            // 解析响应JSON并输出最新版本号
+            const release = JSON.parse(request.responseText);
+            const latestVersion = release.tag_name;
+            app.$data.newest_version = latestVersion;
+            // alert(`Latest version is ${latestVersion}`);
+        };
+        request.onerror = function() {
+            console.error('Error: failed to get latest version.');
+        };
+        request.send();
+    },
+    methods: {
+        changeLang(lang = 'zh') {
+            this.init = false;
+            this.lang = lang;
+        },
+        startDesign(lang, with_data = false) {
+            if (with_data) {
+                console.log(this.user_data_folder)
+                if (this.user_data_folder == null || this.user_data_folder == "") {
+                    if (lang == 'zh') {
+                        alert("请指定用户信息目录");
+                    } else {
+                        alert("Please specify the user information directory");
+                    }
+                    return;
+                }
+                window.electronAPI.startDesign(lang, this.user_data_folder);
+            } else {
+                window.electronAPI.startDesign(lang);
+            }
+        },
+        startInvoke(lang) {
+            window.electronAPI.startInvoke(lang);
+        }
+    }
+}).use(i18n).mount('#app')
+console.log(i18n.global.t('test'))
