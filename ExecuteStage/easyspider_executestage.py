5853d2da005694764ef23eb0491a570da88312b4
Naibo Wang <naibowang@u.nus.edu>
2023-05-27 19:49:41 +0800

    Linux OCR Hint

--- ExecuteStage/easyspider_executestage.py
+++ ExecuteStage/easyspider_executestage.py
@@ -867,7 +867,14 @@ class BrowserThread(Thread):
                 except Exception as e:
                     content = "OCR Error"
                     print("To use OCR, You need to install Tesseract-OCR and add it to the environment variable PATH (need to restart EasySpider after you put in PATH): https://tesseract-ocr.github.io/tessdoc/Installation.html")
-                    print("要使用OCR识别功能，你需要安装Tesseract-OCR并将其添加到环境变量PATH中（添加后需重启EasySpider）：https://blog.csdn.net/u010454030/article/details/80515501\nhttps://www.bilibili.com/video/BV1xz4y1b72D/")
+                    if sys.platform == "win32":
+                        print("要使用OCR识别功能，你需要安装Tesseract-OCR并将其添加到环境变量PATH中（添加后需重启EasySpider）：https://blog.csdn.net/u010454030/article/details/80515501\nhttps://www.bilibili.com/video/BV1xz4y1b72D/")
+                    elif sys.platform == "darwin":
+                        print("要使用OCR识别功能，你需要安装Tesseract-OCR并将其添加到环境变量PATH中（添加后需重启EasySpider）：https://zhuanlan.zhihu.com/p/146044810")
+                    elif sys.platform == "linux":
+                        print("要使用OCR识别功能，你需要安装Tesseract-OCR并将其添加到环境变量PATH中（添加后需重启EasySpider）：https://zhuanlan.zhihu.com/p/420259031")
+                    else:
+                        print("要使用OCR识别功能，你需要安装Tesseract-OCR并将其添加到环境变量PATH中（添加后需重启EasySpider）：https://blog.csdn.net/u010454030/article/details/80515501\nhttps://www.bilibili.com/video/BV1xz4y1b72D/")
             elif p["contentType"] == 9:
                 content = self.execute_code(2, p["JS"], p["JSWaitTime"], element)
             elif p["contentType"] == 10: # 下拉框选中的值
