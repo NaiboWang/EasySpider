21941e635e80e73bf1f1a91ce8842e37943e1ebb
Naibo_Mac <naibowang@u.nus.edu>
2023-06-08 02:41:06 +0800

    Update

--- ExecuteStage/generateExecutable_Macos.sh
+++ ExecuteStage/generateExecutable_Macos.sh
@@ -1,7 +1,7 @@
 rm -r build
 rm -r dist
 pyinstaller -F --icon=favicon.ico easyspider_executestage.py
-rm ../Releases/EasySpider_MacOS_all_arch/easyspider_executestage
+rm ../.temp_to_pub/EasySpider_MacOS_all_arch/easyspider_executestage
 rm ../ElectronJS/easyspider_executestage
-cp dist/easyspider_executestage ../Releases/EasySpider_MacOS_all_arch/easyspider_executestage
+cp dist/easyspider_executestage ../.temp_to_pub/EasySpider_MacOS_all_arch/easyspider_executestage
 # mv dist/easyspider_executestage ../ElectronJS/easyspider_executestage
