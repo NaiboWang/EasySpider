42dc5350c4cca970502eae542c8f60b2afd0ee88
naibo <naibowang@u.nus.edu>
2023-08-14 01:30:40 +0800

    Win32 0.5.0

--- ExecuteStage/generateEXE_win32.cmd
+++ ExecuteStage/generateEXE_win32.cmd
@@ -1,5 +1,5 @@
 rmdir /s /q build
 rmdir /s /q dist
-pyinstaller -F --icon=favicon.ico easyspider_executestage.py
+pyinstaller -F --icon=favicon.ico --add-data "C:\Users\Naibo\AppData\Local\Programs\Python\Python38-32\Lib\site-packages\onnxruntime\capi\onnxruntime_providers_shared.dll;onnxruntime\capi" --add-data "C:\Users\Naibo\AppData\Local\Programs\Python\Python38-32\Lib\site-packages\ddddocr\common.onnx;ddddocr" easyspider_executestage.py
 del ..\ElectronJS\chrome_win32\easyspider_executestage.exe
 copy dist\easyspider_executestage.exe ..\ElectronJS\chrome_win32\easyspider_executestage.exe
