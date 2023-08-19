7ffc10fb1ecf4276f821ca15dc2d5cba969fa0ac
naibo <naibowang@foxmail.com>
2023-08-19 13:32:12 +0800

    Bug fix about invoke path

--- ElectronJS/execute.bat
+++ ElectronJS/execute.bat
@@ -2,5 +2,10 @@
 if exist EasySpider (
    start EasySpider/resources/app/chrome_win64/easyspider_executestage.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
- start chrome_win64/easyspider_executestage.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+if exist resources (
+   cd ../
+   start EasySpider/resources/app/chrome_win64/easyspider_executestage.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+  ) else (
+   start chrome_win64/easyspider_executestage.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+  )
 )
