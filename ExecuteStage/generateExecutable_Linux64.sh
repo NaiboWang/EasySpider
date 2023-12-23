2eeeab89f6ae17e3f073bfa3a1aa1e7701efd1c3
Naibo Wang <naibowang@u.nus.edu>
2023-12-24 03:45:44 +0800

    Debug of Linux

--- ExecuteStage/generateExecutable_Linux64.sh
+++ ExecuteStage/generateExecutable_Linux64.sh
@@ -1,5 +1,6 @@
 rm -r build
 rm -r dist
-pyinstaller -F --icon=favicon.ico --add-data "/home/naibo/miniconda3/lib/python3.8/site-packages/onnxruntime/capi/onnxruntime_pybind11_state.cpython-38-x86_64-linux-gnu.so:onnxruntime/capi"  --add-data "/home/naibo/miniconda3/lib/python3.8/site-packages/ddddocr/common_old.onnx:ddddocr" easyspider_executestage.py
+# Python一定要是3.11版本，不然会出现浏览器弹出崩溃的错误！！！原来使用的3.8，崩溃原因未知。
+pyinstaller -F --add-data "/home/naibo/miniconda3/envs/easyspider/lib/python3.11/site-packages/onnxruntime/capi/onnxruntime_pybind11_state.cpython-311-x86_64-linux-gnu.so:onnxruntime/capi" --add-data "/home/naibo/miniconda3/envs/easyspider/lib/python3.11/site-packages/ddddocr/common_old.onnx:ddddocr" easyspider_executestage.py
 rm ../ElectronJS/chrome_linux64/easyspider_executestage
 cp dist/easyspider_executestage ../ElectronJS/chrome_linux64/easyspider_executestage
