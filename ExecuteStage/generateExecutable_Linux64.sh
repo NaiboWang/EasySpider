fce97dec61319e1df573b62cbfcdac48527bdf02
Naibo Wang <naibowang@u.nus.edu>
2024-04-22 07:44:19 +0800

    Linux

--- ExecuteStage/generateExecutable_Linux64.sh
+++ ExecuteStage/generateExecutable_Linux64.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 rm -r build
 rm -r dist
+# 一定要先source一下，不然会出现找不到conda命令的错误！！！
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate easyspider
 # Python一定要是3.11版本，不然会出现浏览器弹出崩溃的错误！！！原来使用的3.8，崩溃原因未知。
 pyinstaller -F --add-data "/home/naibo/miniconda3/envs/easyspider/lib/python3.11/site-packages/onnxruntime/capi/onnxruntime_pybind11_state.cpython-311-x86_64-linux-gnu.so:onnxruntime/capi" --add-data "/home/naibo/miniconda3/envs/easyspider/lib/python3.11/site-packages/ddddocr/common_old.onnx:ddddocr" easyspider_executestage.py
 rm ../ElectronJS/chrome_linux64/easyspider_executestage
